92ef227a5d6d91d460c21f714315a5969688d1ae
gilias24-meet <ashelgal@gmail.com>
2024-07-29 12:09:39 +0300

    new

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask, render_template, url_for, redirect, request
-from flask import session  
-import pyrebase 
+from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
+from flask import session as login_session
+import random
+import pyrebase
 
 app = Flask( __name__ , template_folder='templates',  static_folder='static'  )
+
+firebaseConfig = {
+  "apiKey": "AIzaSyAAL4ipnYtGe8RGsmH4VUgs5RYwgP64X_Y",
+  "authDomain": "giftapp-ad937.firebaseapp.com",
+  "databaseURL": "https://giftapp-ad937-default-rtdb.europe-west1.firebasedatabase.app",
+  "projectId": "giftapp-ad937",
+  "storageBucket": "giftapp-ad937.appspot.com",
+  "messagingSenderId": "904198728084",
+  "appId": "1:904198728084:web:3bcf00efde3e24626fc781",
+  'measurementId': "G-BF70XBJ7YD"
+};
+
+
+
 app.config['SECRET_KEY'] = 'super-secret-key'
-firebase = pyrebase.initialize_app(Config)
+firebase = pyrebase.initialize_app(firebaseConfig) 
 auth = firebase.auth()
-db =firebase.database()
+db = firebase.database()
 
 
 if __name__ == "__main__":  

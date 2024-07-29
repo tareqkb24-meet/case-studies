c94718fa596197f2c84a817c8ca7ea4067298674
tareqkb24-meet <t32185278@gmail.com>
2024-07-29 13:06:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,9 @@ firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
 db = firebase.database()
 
+@app.route("/home")
+def home():
+  return render_template("index.html")
 
 if __name__ == "__main__":  
   app.run(debug=True)

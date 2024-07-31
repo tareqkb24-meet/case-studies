ee44c90703b98e06a06be55ac84d6349be1061d1
gilias24-meet <ashelgal@gmail.com>
2024-07-31 09:23:07 +0300

    /

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ db = firebase.database()
 @app.route('/home', methods=['GET', 'POST'])
 def index():
   if request.method == "POST":
-    print('lala')
     if "change_mode" in request.form:
       print(login_session['mode'])
       if login_session['mode'] == "signup":
@@ -36,13 +35,16 @@ def index():
       print(login_session['mode'])
 
     else:
-      if login_session['mode'] == "signup":
-        email = request.form["email"]
-        username = request.form['username']
-        password = request.form['password']
-        pair = "None" 
-        choice = request.form['choice']
-        login_session["user"] = auth.create_user_with_email_and_password(email, password)
+       if login_session['mode'] == "signup":
+        try:
+          email = request.form["email"]
+          username = request.form['username']
+          password = request.form['password']
+          pair = "None" 
+          choice = request.form['choice']
+          login_session["user"] = auth.create_user_with_email_and_password(email, password)
+        except:
+          return render_template("index.html")
         user = {'username': username, 'email':email, 'password':password, "pair": pair, "choice": choice}
         UID = login_session["user"]['localId']
         login_session["user"]["choice"] = choice
@@ -53,31 +55,41 @@ def index():
               db.child("user").child(i) .update({'pair':UID})
               return redirect(url_for("chat"))
         return('not pair')
-      if mode == "signin":
-        #signin
-        pass
-    return render_template("index.html", mode=login_session['mode'])
+        if login_session['mode'] == "login":
+          try:
+            email = request.form["email"]
+            password = request.form['password']
+            login_session["user"] = auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for("chat"))
+          except:
+            return render_template("index.html")
+    if login_session['mode'] == 'signup':
+      not_mode = 'login'
+    else:
+      not_mode = 'signup'
+    return render_template("index.html", mode=login_session['mode'], not_mode=not_mode)
   login_session['mode'] = "signup"
-  return render_template("index.html", mode=login_session['mode'])
+  not_mode = "login"
+  return render_template("index.html", mode=login_session['mode'], not_mode=not_mode)
 
 
 @app.route ('/chat', methods = ['GET', 'POST']) 
-def chat ():
+def chat():
+  UID = login_session["user"]['localId']
+
   if request.method == 'POST':
     studentInput = request.form['student']
-    UID = login_session["user"]['localId']
     msg = {"user": UID, "input" : studentInput}
     db.child("user").child(UID).child("chat").push(msg)
 
-    return render_template('chat.html')
+    pair = db.child("user").child(UID).child('pair').get().val()
 
-  return render_template('chat.html') 
+    db.child("user").child(pair).child("chat").push(msg)
+
+  conversation = db.child("user").child(UID).child("chat").get().val()
+  print(conversation)
+  return render_template('chat.html', conversation = conversation) 
  
-
-
-
-
-
 
 if __name__ == "__main__":  
   app.run(debug=True)
